03560f72dd4a069d745126073735bb64d2289e3a
Aman Choudhri <amanchoudhri@gmail.com>
2024-06-24 11:18:16 -0400

    Clearing changes on flatiron remote machine

--- nvim/.config/nvim/after/plugin/keymap/init.lua
+++ nvim/.config/nvim/after/plugin/keymap/init.lua
@@ -3,3 +3,7 @@ vim.keymap.set('n', '<C-h>', '<C-w>h', {})
 vim.keymap.set('n', '<C-j>', '<C-w>j', {})
 vim.keymap.set('n', '<C-k>', '<C-w>k', {})
 vim.keymap.set('n', '<C-l>', '<C-w>l', {})
+
+-- Recenter after using <C-d> and <C-u>
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { remap = false })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { remap = false })
